f0a4421be0503c6ad75bc52286561a8c4519adcb
gustavneustadt <gustav@einfach-neu.de>
2022-08-19 08:26:16 +0200

    fixed svg line firefox support

--- src/helper/SvgLine.svelte
+++ src/helper/SvgLine.svelte
@@ -27,12 +27,15 @@
 	
 	export let strokeWidth: number = 1
 	
-	$: x2 = x2 ? x2 : x1
-	$: y2 = y2 ? y2 : y1
+	$: pos = {
+		x1: x1,
+		x2: x2 ?? x1,
+		y1: y1,
+		y2: y2 ?? y1
+	}
 	
-	$: path = `M ${x1} ${y1}, L ${x2} ${y2}`
-	
-	
+	$: path = `M ${pos.x1} ${pos.y1} L ${pos.x2} ${pos.y2}`
+
 	const crossProduct = (v1: Vector, v2: Vector): Vector => {
 		return [
 			v1[1]*v2[0] - v1[0]*v2[1],
@@ -74,16 +77,16 @@
 			const py = position[1] - size / 2
 			
 			return `
-				M ${px} ${py},
-				C ${px+handleLength} ${py}, ${px+size/2} ${py+size/2-handleLength}, ${px+size/2} ${py+size/2}
-				C ${px+size/2} ${py+size/2+handleLength}, ${px+handleLength} ${py+size}, ${px} ${py+size}
-				C ${px - handleLength} ${py+size}, ${px - size/2} ${py+size/2 + handleLength}, ${px - size/2} ${py+size/2}
-				C ${px - size/2} ${py+size/2 - handleLength}, ${px - handleLength} ${py}, ${px} ${py}
+				M ${px} ${py}
+				C ${px+handleLength} ${py} ${px+size/2} ${py+size/2-handleLength} ${px+size/2} ${py+size/2}
+				C ${px+size/2} ${py+size/2+handleLength} ${px+handleLength} ${py+size} ${px} ${py+size}
+				C ${px - handleLength} ${py+size} ${px - size/2} ${py+size/2 + handleLength} ${px - size/2} ${py+size/2}
+				C ${px - size/2} ${py+size/2 - handleLength} ${px - handleLength} ${py} ${px} ${py}
 			`
 		}],
 		[LineEnd.LineOneSide, function(position: Point = [0, 0]): string {
-			const p1: [number, number] = [x1, y1]
-			const p2: [number, number] = [x2, y2]
+			const p1: [number, number] = [pos.x1, pos.y1]
+			const p2: [number, number] = [pos.x2, pos.y2]
 			
 			const v: [number, number] = p2[0] === p1[0] && p2[1] === p1[1] ? [lineEndHeight / 2, 0] : unitVector(pointToVector(p1, p2), lineEndHeight)
 			
@@ -91,11 +94,11 @@
 			
 			const [rx1, ry1] = rotate(v, Math.PI / 2)
 			
-			return `M ${px + rx1} ${py + ry1}, L ${px} ${py}`
+			return `M ${px + rx1} ${py + ry1} L ${px} ${py}`
 		}],
 		[LineEnd.Line, function(position: Point = [0, 0]): string {
-			const p1: [number, number] = [x1, y1]
-			const p2: [number, number] = [x2, y2]
+			const p1: [number, number] = [pos.x1, pos.y1]
+			const p2: [number, number] = [pos.x2, pos.y2]
 			
 			const v: [number, number] = p2[0] === p1[0] && p2[1] === p1[1] ? [lineEndHeight / 2, 0] : unitVector(pointToVector(p1, p2), lineEndHeight / 2)
 		
@@ -104,26 +107,24 @@
 			const [rx1, ry1] = rotate(v, Math.PI / 2)
 			const [rx2, ry2] = rotate([rx1, ry1], Math.PI)
 			
-			return `M ${px + rx1} ${py + ry1}, L ${px + rx2} ${py + ry2}`
+			return `M ${px + rx1} ${py + ry1} L ${px + rx2} ${py + ry2}`
 		}],
 		[LineEnd.Arrow, function(position: Point = [0, 0]): string {
 			return ""
 		}]
 	])
-
-	
 </script>
 
 <style>
 
 </style>
 
-<path d={path} stroke-width={strokeWidth} stroke="currentColor"/>
-	
-{#if end != LineEnd.None}
-	<path class="line-end end"  d={lineEndPaths.get(end)([x2, y2])} stroke-width={strokeWidth} stroke="currentColor" fill="none"/>
-{/if}
+	<path d={path} stroke-width={strokeWidth} stroke="currentColor"/>
 
-{#if start != LineEnd.None}
-	<path class="line-end start" d={lineEndPaths.get(start)([x1, y1])} stroke-width={strokeWidth} stroke="currentColor" fill="none"/>
-{/if}
+
+	{#if end != LineEnd.None}
+		<path class="line-end end"  d={lineEndPaths.get(end)([pos.x2, pos.y2])} stroke-width={strokeWidth} stroke="currentColor" fill="none"/>
+	{/if}
+	{#if start != LineEnd.None}
+		<path class="line-end start" d={lineEndPaths.get(start)([pos.x1, pos.y1])} stroke-width={strokeWidth} stroke="currentColor" fill="none"/>
+	{/if}
