d262402bf4474705d2dff548cc9c4392dacc7589
gustavneustadt <gustav@einfach-neu.de>
2022-08-19 08:26:52 +0200

    added typescript to axis helper and prop spread

--- src/helper/AxisHelper.svelte
+++ src/helper/AxisHelper.svelte
@@ -1,7 +1,7 @@
-<script>
+<script lang="ts">
 	import * as d3 from "d3"
 	export let axis
-	let axisWrapper
+	let axisWrapper: SVGElement
 	
 	$: {
 		d3.select(axisWrapper).call(axis)
@@ -12,5 +12,5 @@
 
 </style>
 
-<g bind:this={axisWrapper}>
+<g bind:this={axisWrapper} {...$$props}>
 </g>
