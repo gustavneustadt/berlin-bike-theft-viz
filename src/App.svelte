c0f48205bb8ec9580fed33a535cb89a0e44fcc64
gustavneustadt <gustav@einfach-neu.de>
2022-08-20 00:00:13 +0200

    added credits

--- src/App.svelte
+++ src/App.svelte
@@ -196,6 +196,69 @@ author {
 	letter-spacing: .1rem;
 	text-align: center;
 }
+
+.credits p {
+	width: 100%;
+	color: var(--colorTextPrimary);
+	line-height: 1.8;
+}
+.credits {
+	width: 100%;
+	background: var(--colorTextDarkMuted);
+	padding: 10rem 0;
+	z-index: 100;
+	position: relative;
+	min-height: 100vh;
+	box-sizing: border-box;
+}
+
+.credits .text-wrapper {
+	width: 32rem;
+	max-width: calc(100vw - 2rem);
+	margin: 0 auto;
+}
+
+.credits h3 {
+	margin: 0 auto 1.5rem;
+	width: 100%;
+	color: var(--colorText);
+	font-weight: 600;
+	font-size: 1.4rem;
+}
+
+.credits h4 {
+	font-variant-caps: all-small-caps;
+	color: var(--colorText);
+	letter-spacing: .1rem;
+	font-weight: 400;
+	font-size: 1.2rem;
+	margin: 2rem 0 1rem;
+}
+
+.credits .source {
+	margin: 1rem 0;
+	color: var(--colorTextPrimaryMuted);
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+.credits .source .link {
+	color: var(--colorAccentPrimaryMuted);
+}
+
+.credits .source .link:hover {
+	color: var(--colorAccentPrimary);
+}
+.credits .source .title {
+	font-size: 1rem;
+}
+
+.credits .source .desc {
+	font-variant-caps: all-small-caps;
+	letter-spacing: .1rem;
+	opacity: .8;
+}
 </style>
 
 {#if data != undefined}
@@ -218,5 +281,81 @@ author {
 	</header>
 	<main>
 		<ContentController />
+			
 	</main>
+	<div class="credits">
+		<div class="text-wrapper">
+			<h3>
+				Credits
+			</h3>
+			<p>
+				This data visualisation project is one of 6 pojects resulted from a course I took at the University of Applied Sciences in Potsdam (FHP) by Lucas Vogel: <em>Citizen Science — Stadtdaten visualisieren</em>.
+			</p>
+			<div class="source">
+				<div class="desc">concept, design & development</div>
+				<div class="title">Gustav Neustadt</div>
+				<a class="link" href="https://twitter.com/gustavneustadt" target="_blank">Twitter</a>
+			</div>
+			
+			<div class="source">
+				<div class="desc">supervision</div>
+				<div class="title">Lucas Vogel</div>
+				<a class="link" href="https://twitter.com/soyvogelino" target="_blank">Twitter</a>
+			</div>
+			
+			<h4>Sources</h4>
+			<div class="source">
+				<div class="desc">Data source</div>
+				<div class="title">Fahrraddiebstahl in Berlin</div>
+				<a class="link" href="https://daten.berlin.de/datensaetze/fahrraddiebstahl-berlin" target="_blank">Berlin Open Data</a>
+			</div>
+			
+			<div class="source">
+				<div class="desc">Font</div>
+				<div class="title"><em>TheMix</em> by Luc(as) de Groot</div>
+				<a class="link" href="http://www.lucasfonts.com/fonts/the-mix" target="_blank">LucasFonts</a>
+			</div>
+			
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Attempted Bike Theft - Angle Grinder - 02.15.19</div>
+				<a class="link" href="https://www.youtube.com/watch?v=069iyliznmI" target="_blank">youtube.com/Will Press</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Bike Theft - Cutting Kryptonite with a battery powered angle grinder</div>
+				<a class="link" href="https://www.youtube.com/watch?v=kcHOVSsfXQ0" target="_blank">youtube.com/David de Sotomayor</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Silke's Giant Explore e bike theft 22 Oct 2019</div>
+				<a class="link" href="https://www.youtube.com/watch?v=0DhICLwxfZY" target="_blank">youtube.com/Mark Cresswell</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Security footage of Portland bike thefts</div>
+				<a class="link" href="https://www.youtube.com/watch?v=dWvM_ND9CI4" target="_blank">youtube.com/KOIN 6</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Sparks Fly During Attempted Bike Theft</div>
+				<a class="link" href="https://www.youtube.com/watch?v=G0ohO91xf1w" target="_blank">youtube.com/Stealth Monitoring</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">Caught On Camera: E-Bike Stolen In Brooklyn</div>
+				<a class="link" href="https://www.youtube.com/watch?v=lFL8gW0-tLw" target="_blank">youtube.com/CBS New York</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">guy tries stealing Eva's bike with an angle grinder</div>
+				<a class="link" href="https://www.youtube.com/watch?v=GNe6nyMfT3w" target="_blank">youtube.com/Charvak Karpe</a>
+			</div>
+			<div class="source">
+				<div class="desc">Video Clip</div>
+				<div class="title">My Bike Being Stolen to Habanera (Carmen)</div>
+				<a class="link" href="https://www.youtube.com/watch?v=EhPclNBlNSQ" target="_blank">youtube.com/Someone YouDontKnow</a>
+			</div>
+		</div>
+	</div>
 {/if}
